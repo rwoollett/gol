0f8b0d786c7a60a981cfec921ae471fb62c176c6
Rodney Woollett <rodney.woollett@gmail.com>
2024-12-25 20:19:58 +1300

    Init GOL repo

--- src/prisma/migrations/20230810043041_connect_default_false/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Client" ALTER COLUMN "connected" SET DEFAULT false;
--- src/prisma/migrations/20241201040738_disconnectat_for_client_object/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Client" ADD COLUMN     "disconnectedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
