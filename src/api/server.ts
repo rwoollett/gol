f45dd7bf995e39e77244b522d03baa5cb3ada76a
Rodney Woollett <rodney.woollett@gmail.com>
2025-01-05 14:29:17 +1300

    Use extended for express jon urlencoded

--- src/api/server.ts
+++ src/api/server.ts
@@ -81,7 +81,7 @@ function start() {
   server.start().then(() => {
     console.log('setting /grapghql');
     app.use(express.json({ limit: '25mb' }));
-    app.use(express.urlencoded({ limit: '25mb' }));
+    app.use(express.urlencoded({ limit: '25mb', extended: true }));
     app.use('/graphql',
       cors<cors.CorsRequest>(),
       bodyParser.json(),
